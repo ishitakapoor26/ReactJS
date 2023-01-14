9b54635e28217fcd8d1fe470fe6093cadc3bdc98
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-01-14 23:20:37 +0530

    Practice code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import Message from './components/Message';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        {/* <Greet firstname= 'Ishita' lastname='Kapoor'>
+          <p>This is a children prop.</p>
+          </Greet>
+        <Welcome firstname='Ishita'/> */}
+        <Message />
+        {/* <Hello /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Greet.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1>Hello Ishita</h1>
+// }
+
+const Greet = (props) => {
+  console.log(props);
+  return(
+    <div>
+
+      <h1>Hello {props.firstname} {props.lastname}!</h1>
+        {props.children}
+    
+    </div>
+      
+  )
+};
+// export const Greet = () => <h1>Hello Ishita!</h1>
+
+export default Greet;
