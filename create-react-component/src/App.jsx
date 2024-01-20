a02e69452abc1577bc913672e445b0ec642dd7e4
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-01-20 22:43:54 +0530

    components

---
+++ create-react-component/src/App.jsx
@@ -0,0 +1,19 @@
+// import Button from "./heyButton";
+// import Hello from "./Hello";
+import Random from "./Random";
+
+function App(){
+  return <div>
+    <h4 style={{'background-color': '#776691'}}>
+    {/* Hello, this is Ishita! */}
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    </h4>
+    {/* <Button></Button> */}
+    {/* <Hello></Hello> */}
+    </div>
+}
+
+export default App;
