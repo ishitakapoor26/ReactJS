a86868d3c22ed014c9c769703c494a64aa0ec110
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-01-21 03:28:04 +0530

    Project 1

---
+++ Projects/1-todo-app-version-1/src/Components/TodoItem2.jsx
@@ -0,0 +1,17 @@
+function TodoItem2() {
+  return (
+    <div className="container">
+      <div className="row kg-row">
+        <div className="col-4">Buy Milk</div>
+        <div className="col-4">04/10/2023</div>
+        <div className="col-2">
+          <button type="button" className="btn btn-danger kg-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem2;
