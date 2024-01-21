9eb7a67749e21a536a690b32f6419efb113c646c
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-01-22 00:02:47 +0530

    Conditional Rendering

--- Fragments/fragment_intro/src/App.jsx
+++ Fragments/fragment_intro/src/App.jsx
@@ -1,9 +1,15 @@
 function App() {
   let mpp = ["Dal", "Roti", "Rice"];
 
+  // Conditional Rendering
+  // if (mpp.length === 0) {
+  //   return <h3>I am still hungry!</h3>;
+  // }
+
   return (
     <>
       <h1>Healthy Food</h1>
+      {mpp.length === 0 ? <h3>I am still hungry!</h3> : null}
       <ul className="list-group">
         {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
