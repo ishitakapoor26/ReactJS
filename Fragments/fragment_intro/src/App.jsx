2a1bc157c78757414c9d075513ab2233ed9cd440
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-01-23 00:37:59 +0530

    Passing Children

--- Fragments/fragment_intro/src/App.jsx
+++ Fragments/fragment_intro/src/App.jsx
@@ -1,5 +1,6 @@
 import FoodItems from "./Components/FoodItems";
 import ErrorMessage from "./Components/ErrorMessage";
+import Container from "./Components/Container";
 
 function App() {
   let mpp = ["Dal", "Roti", "Rice"];
@@ -10,14 +11,22 @@ function App() {
 
   return (
     <>
-      <h1 className="food-heading">Healthy Food</h1>
-      <ErrorMessage items={mpp}></ErrorMessage>
-      <FoodItems items={mpp}></FoodItems>
-      {/* <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li> */}
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage items={mpp}></ErrorMessage>
+        <FoodItems items={mpp}></FoodItems>
+        {/* <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li> */}
+      </Container>
+      <Container>
+        <p>
+          Above is the list of healthy foods that are good for your health and
+          well being.
+        </p>
+      </Container>
     </>
   );
 }
--- Fragments/fragment_intro/src/Components/Item.jsx
+++ Fragments/fragment_intro/src/Components/Item.jsx
@@ -3,7 +3,8 @@ import styles from "./Item.module.css";
 // eslint-disable-next-line react/prop-types
 let Item = ({ foodItem }) => {
   return (
-    <li className={`${styles["kg-item"]} list-group-item}`}>
+    <li className="list-group-item">
+      {/* <li className={`"list-group-item" ${styles["kg-item"]}`}> */}
       <span className={styles["kg-span"]}>{foodItem}</span>
     </li>
   );
