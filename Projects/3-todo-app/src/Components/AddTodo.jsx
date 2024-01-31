6815309227fd84f713ba0b22d846549a014fea57
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-01-31 14:25:13 +0530

    useRef

--- Projects/3-todo-app/src/Components/AddTodo.jsx
+++ Projects/3-todo-app/src/Components/AddTodo.jsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 // eslint-disable-next-line react/prop-types
 function AddTodo({ addToDoHandler }) {
-  const [todoName, setName] = useState();
-  const [todoDate, setDate] = useState();
+  // const [todoName, setName] = useState();
+  // const [todoDate, setDate] = useState();
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
+  // const handleNameChange = (event) => {
+  //   setName(event.target.value);
+  // };
 
-  const handleDateChange = (event) => {
-    setDate(event.target.value);
-  };
+  // const handleDateChange = (event) => {
+  //   setDate(event.target.value);
+  // };
 
-  let handleAddButton = (event) => {
+  // let handleAddButton = (event) => {
+  //   event.preventDefault();
+  //   addToDoHandler(todoName, todoDate);
+  //   setDate("");
+  //   setName("");
+  // };
+
+  const todoNameElement = useRef();
+  const todoDateElement = useRef();
+
+  const handleAddButton = (event) => {
     event.preventDefault();
-    addToDoHandler(todoName, todoDate);
-    setDate("");
-    setName("");
+    const todoName = todoNameElement.current.value;
+    const dueDate = todoDateElement.current.value;
+    todoDateElement.current.value = "";
+    todoNameElement.current.value = "";
+    addToDoHandler(todoName, dueDate);
   };
 
   return (
@@ -26,21 +38,12 @@ function AddTodo({ addToDoHandler }) {
         <div className="col-4">
           <input
             type="text"
-            value={todoName}
+            ref={todoNameElement}
             placeholder="Enter to-do here"
-            onChange={(event) => {
-              handleNameChange(event);
-            }}
           />
         </div>
         <div className="col-4">
-          <input
-            type="date"
-            value={todoDate}
-            onChange={(event) => {
-              handleDateChange(event);
-            }}
-          />
+          <input type="date" ref={todoDateElement} />
         </div>
         <div className="col-1">
           <button type="submit" className="btn btn-success kg-button">
