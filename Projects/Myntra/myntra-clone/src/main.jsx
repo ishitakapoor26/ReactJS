7468dee881f1e4324ccece474aabf2b4c538abc8
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-02-27 23:31:43 +0530

    Myntra project added

---
+++ Projects/Myntra/myntra-clone/src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./routes/App.jsx";
+import "./index.css";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
---
+++ Projects/Myntra/myntra-clone/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <main>
+        <div className="items-container"></div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
