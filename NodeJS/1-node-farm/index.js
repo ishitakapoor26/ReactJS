43d988afea9707414fb35878bae861f74c1f84a4
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-04-14 22:55:24 +0530

    updates

--- NodeJS/1-node-farm/index.js
+++ NodeJS/1-node-farm/index.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const slugify = require("slugify");
+const replaceTemplate = require("./modules.js/replaceTemplate");
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`);
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`);
@@ -9,19 +11,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.toString().replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
