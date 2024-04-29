365d8e59ff671675a651de59f5ba901d28aa1d85
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-04-29 18:11:38 +0530

    CRUD updates

--- NodeJS/natours/app.js
+++ NodeJS/natours/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fs = require("fs");
-
 const app = express();
+
+// Middleware is just a function that can modify the incoming data
+app.use(express.json());
 
 // Read the JSON file synchronously
 const tours = JSON.parse(
@@ -15,6 +17,50 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
+  console.log(tours[tours.length - 1].id + 1);
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  let newId;
+  if (tours.length === 0) {
+    newId = 1; // If tours array is empty, start from 1
+  } else {
+    newId = tours[tours.length - 1].id + 1; // Increment the last ID
+  }
+
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        // Handle error
+        console.error(err);
+        res.status(500).json({
+          status: "error",
+          message: "Failed to write tour data.",
+        });
+      } else {
+        res.status(201).json({
+          status: "success",
+          data: {
+            tours: newTour,
+          },
+        });
+      }
+    }
+  );
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "error",
+      message: "Invalid ID",
+    });
+  }
 });
 
 app.listen(3000, () => {
