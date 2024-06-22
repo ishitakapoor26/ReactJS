5a3de8e9d57b33e49d5fff36c6173e2d16bb6eb2
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-06-22 15:43:14 +0530

    NodeJS error handling

--- NodeJS/natours/app.js
+++ NodeJS/natours/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const morgan = require("morgan"); // Import morgan
+const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 
@@ -12,5 +14,18 @@ app.use(express.json());
 
 // Routes
 app.use("/api/v1/tours", tourRouter);
+
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // (err.status = "fail"), (err.statusCode = 404);
+  // next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
