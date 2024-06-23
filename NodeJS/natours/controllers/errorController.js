7dd2b5a4c0f97d58c29f5919691d395ba4e2b73e
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-06-23 19:35:48 +0530

    Handling Duplicate Database Fields

--- NodeJS/natours/controllers/errorController.js
+++ NodeJS/natours/controllers/errorController.js
@@ -2,7 +2,26 @@ const AppError = require("./../utils/appError");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 404);
+  return new AppError(message, 400); // CastError is typically a 400 Bad Request
+};
+
+const handleDuplicateErrorDB = (err) => {
+  let value;
+  if (err.errmsg) {
+    const match = err.errmsg.match(/(["'])(\\?.)*?\1/);
+    if (match) {
+      console.log(match);
+      value = match[0];
+    }
+  }
+
+  // Debugging log to check the error object structure
+  console.log("Duplicate error debug info:", err);
+
+  const message = value
+    ? `Duplicate field value: ${value}. Please use another value!`
+    : "Duplicate field value. Please use another value!";
+  return new AppError(message, 400); // Duplicate error is typically a 400 Bad Request
 };
 
 const sendErrorDev = (err, res) => {
@@ -24,7 +43,7 @@ const sendErrorProd = (err, res) => {
 
     // Programming or other unknown error: don't leak error details
   } else {
-    console.log("ERROR :(", err);
+    console.error("ERROR :(", err);
 
     res.status(500).json({
       status: "error",
@@ -40,11 +59,11 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
-    next(err);
   } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
+    let error = { ...err, name: err.name, message: err.message }; // Ensure name and message are copied
 
     if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
 
     sendErrorProd(error, res);
   }
