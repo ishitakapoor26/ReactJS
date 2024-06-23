216eac87b60324341e448c27b455a649cb2539c9
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-06-23 18:54:33 +0530

    Error Handling dev vs production

--- NodeJS/natours/controllers/errorController.js
+++ NodeJS/natours/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 404);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -33,7 +40,12 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
+    next(err);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
