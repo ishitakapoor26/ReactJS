609920d15e03d8c62832c95ea271e18c7cd895a3
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-11-08 23:21:09 +0530

    Data Sanitization

--- NodeJS/natours/app.js
+++ NodeJS/natours/app.js
@@ -8,6 +8,8 @@ const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 // Global Middleware to log HTTP requests (dev format)
 
@@ -19,6 +21,13 @@ app.use(morgan("dev"));
 
 // Middleware to parse JSON bodies
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against nosql query injection
+
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 
 // Set request limits
 const limiter = rateLimit({
