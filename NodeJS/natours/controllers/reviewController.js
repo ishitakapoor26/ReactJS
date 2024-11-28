e998fa3f6e59949826702c2b9931df8cf09eb6c9
Ishita Kapoor <ishitakapoor12612@gmail.com>
2024-11-28 21:31:24 +0530

    Review restriction added for a specific tour and user

--- NodeJS/natours/controllers/reviewController.js
+++ NodeJS/natours/controllers/reviewController.js
@@ -1,12 +1,35 @@
 const Review = require("../models/reviewModel");
+const Booking = require("../models/bookingModel");
+const catchAsync = require("../utils/catchAsync");
 const factory = require("./handlerFactory");
+const AppError = require("../utils/appError");
 
 exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.params.id;
+  if (!req.body.user) req.body.user = req.user.id;
   next();
 };
+
+exports.reviewRestrictToBookings = catchAsync(async (req, res, next) => {
+  try {
+    // Query using `findOne` for both user and tour
+    const booking = await Booking.findOne({
+      user: req.body.user,
+      tour: req.body.tour,
+    });
+
+    if (!booking) {
+      return next(
+        new AppError("Tour Booking associated with user does not exist.", 404)
+      );
+    }
+
+    next();
+  } catch (err) {
+    next(err); // Handle errors gracefully
+  }
+});
 
 exports.getAllReviews = factory.getAllOne(Review);
 
--- NodeJS/natours/routes/reviewRouter.js
+++ NodeJS/natours/routes/reviewRouter.js
@@ -11,6 +11,7 @@ router
   .post(
     authController.restrictTo("user"),
     reviewController.setTourUserIds,
+    reviewController.reviewRestrictToBookings,
     reviewController.addReviews
   );
 
